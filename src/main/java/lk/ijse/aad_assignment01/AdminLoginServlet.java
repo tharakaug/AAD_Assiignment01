b53302abeca9c1e28e80a8c2fb458adec9d02922
Kavindu Tharaka <157113740+tharakaug@users.noreply.github.com>
2025-01-24 01:22:22 +0530

    admin servlet

---
+++ src/main/java/lk/ijse/aad_assignment01/AdminLoginServlet.java
@@ -0,0 +1,4 @@
+package lk.ijse.aad_assignment01;
+
+public class AdminLoginServlet {
+}
---
+++ src/main/java/lk/ijse/aad_assignment01/UserDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.aad_assignment01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    private String name;
+    private String email;
+    private String password;
+}
