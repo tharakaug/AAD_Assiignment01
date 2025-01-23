e304cbaf865818ebd58dbab0b33d1c83426af18f
Kavindu Tharaka <157113740+tharakaug@users.noreply.github.com>
2025-01-23 17:31:13 +0530

    user servlet

---
+++ src/main/java/lk/ijse/aad_assignment01/UserServlet.java
@@ -0,0 +1,56 @@
+package lk.ijse.aad_assignment01;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(name = "/userServlet", value = "/user-Servlet")
+
+public class UserServlet extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private DataSource dataSource;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        System.out.println(name+"" +email +"" + password);
+
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name,email, password) VALUES (?,?,?)");
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+
+            int i = preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+
+            if (i > 0) {
+                System.out.println("login success");
+                resp.sendRedirect("index.jsp");
+            } else {
+                System.out.println("login failed");
+                resp.sendRedirect("userLogin.jsp");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+   }
+
+    }
+
+}
